e6e301b8b036e59ceef20e57d96f37daec6e3065
Junior da Silva <jr.a.silva@outlook.com>
2021-10-05 09:05:51 -0300

    Movimenta o jogador

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ const map = [
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
+
+
+function movimenta () {
+
+}
+
+let posicaoJogadorLeft = -165;
+let posicaoJogadorTop = -90;
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === 'ArrowUp'){
+    posicaoJogadorTop -= 15;      
+  } else if (keyName === 'ArrowDown') {    
+    posicaoJogadorTop += 15;
+  } else if (keyName === 'ArrowLeft') {
+    posicaoJogadorLeft -= 15;    
+  } else if (keyName === 'ArrowRight') {    
+    posicaoJogadorLeft += 15;    
+  }
+    document.getElementById("jogador").style.top = posicaoJogadorTop + 'px';
+    document.getElementById("jogador").style.left = posicaoJogadorLeft + 'px';
+
+  // para testes de posição
+document.getElementById("paragrafoPosicaoTop").innerText = "Posição Top do jogador: " + posicaoJogadorTop;
+document.getElementById("paragrafoPosicaoLeft").innerText = "Posição Left do jogador: " + posicaoJogadorLeft;
+// fim teste de posição
+
+});
+
+
